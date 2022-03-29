70f4de47601e62fc1fe234f0eaab6f2d1b4b3d2d
nana-kn <knyazewa2015@mail.ru>
2022-03-29 15:30:03 +0300

    Изменена структура проекта

--- main_page.py
+++
@@ -1,9 +0,0 @@
-from .base_page import BasePage
-from selenium.webdriver.common.by import By
-
-class MainPage(BasePage):
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs) 
-    
-        
-    
