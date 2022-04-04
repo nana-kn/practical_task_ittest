286cf5fd18c794ad57f585ead9c4dd5758783a2d
nana-kn <knyazewa2015@mail.ru>
2022-04-04 19:06:19 +0300

    Добавила проверки в test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,6 @@ from .pages.register_page import RegisterPage
 
 
 class TestRegisterFromMainPage():
-    
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://demowebshop.tricentis.com/"
         main_page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
@@ -12,7 +11,8 @@ class TestRegisterFromMainPage():
         main_page.go_to_login_page()          # выполняем метод страницы — переходим на страницу авторизации
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_elements_for_login()
-    
+
+class TestLoginFromMainPage():    
     def test_guest_can_go_to_register_page(self, browser):
         link = "http://demowebshop.tricentis.com/"
         main_page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
